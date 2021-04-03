0ddf1c88d92f7cde7963eeb72e15c34fb3444be7
Thetelegram212 <80273057+Thetelegram212@users.noreply.github.com>
2021-04-03 20:17:50 +0100

    Add files via upload

---
+++ boo12.py
@@ -0,0 +1,5 @@
+
+print(bool(7))
+print(bool(1))
+print(bool(0))
+print(bool(-1))
---
+++ t.py
@@ -0,0 +1,40 @@
+print('Simple calculator!')
+
+first_number = input('First number? ')
+
+if not first_number.isnumeric():
+    print('Please input a number.')
+    exit()
+operation = input('Operation? ')
+
+second_number = input('Second number? ')
+if not second_number.isnumeric():
+    print('Please input a number.')
+    exit()
+
+first_number = int(first_number)
+second_number = int(second_number)
+
+result = 0
+if operation == '+':
+    result = first_number + second_number
+    label = 'sum'
+elif operation == '-':
+    result = first_number - second_number
+    label = 'difference'
+elif operation == '*':
+    result = first_number * second_number
+    label = 'product'
+elif operation == '/':
+    result = first_number / second_number
+    label = 'quotient'
+elif operation == '**':
+    result = first_number ** second_number
+    label = 'exponent'
+elif operation == '%':
+    result = first_number % second_number
+    label = 'modulus'
+else:
+    print('Operation not recognized.')
+    exit()
+print(label + ' of ' + str(first_number) + ' ' + operation + ' ' + str(second_number) + ' equals ' + str(result))
